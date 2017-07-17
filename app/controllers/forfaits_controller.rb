4807fb09238b492d08ebef9efa8ed74272dfb7ba
Jordan Demmouche <jordand75020@hotmail.fr>
2017-07-18 01:28:52 +0200

    forfaits

--- app/controllers/forfaits_controller.rb
+++ app/controllers/forfaits_controller.rb
@@ -1,5 +1,6 @@
 class ForfaitsController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index]
   def index
+    @forfaits = Forfait.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Forfaits...."
+f = Forfait.new
+f.title = "One page"
+f.description = "Un site 1 page, idéal pour présenter votre concept ou promouvoir un lancement d'application"
+f.price = 499
+f.save
+puts "another one..."
+f = Forfait.new
+f.title = "Multi pages"
+f.description = "Site multi pages responsive design. Optimisé pour le référencement"
+f.price = 1099
+f.save
+puts "another one......"
+f = Forfait.new
+f.title = "Premium"
+f.description = "Site e-commerce avec haut niveau de finitions graphiques, intégration du catalogue"
+f.price = 1499
+f.save
+puts "last one......"
+f = Forfait.new
+f.title = "Prototype&MVP"
+f.description = "Prototype&MVP personnalisés parfait pour le développement de nouveaux business modèles et nouveaux produits."
+f.price = 999
+f.save
+puts "Finishing"
