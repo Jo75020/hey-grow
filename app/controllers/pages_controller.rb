b613af8111030b7197577f3503bf6bf7211371ec
Jordan Demmouche <jordand75020@hotmail.fr>
2017-07-21 16:30:25 +0200

    add stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,6 @@ class PagesController < ApplicationController
 
 
 
-  private
 
-  def user_params
-    # *Strong params*: You need to *whitelist* what can be updated by the user
-    # Never trust user data!
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+
+  private
+
+  def set_user
+    @user = User.find(user_params)
+  end
+
+  def user_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
 end
